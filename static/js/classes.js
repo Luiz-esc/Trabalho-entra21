b4343ae683c083d6a43f75ca84fcdbc9d8d40985
LUIZ EDUARDO SCHLICKMANN DE CARVALHO <luiz.carvalho@alunos.sc.senac.br>
2021-07-07 17:14:26 -0300

    Início do trabalho

---
+++ static/js/classes.js
@@ -0,0 +1,10 @@
+class Turma {
+    constructor(codigoTurma, nomeTurma) {
+        this.codigoTurma = codigoTurma
+        this.nomeTurma = nomeTurma
+        this.alunos = []
+    }
+    cadastrarAluno(aluno) {
+        let aluno = new Aluno()
+    }
+}
