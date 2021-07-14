0cd07af47fa3a04549e4353ec63efab2a3041124
Vinícius Bilk <vinicius.bilk@alunos.sc.senac.br>
2021-07-14 15:26:27 -0300

    ADicionando o value das notas

--- static/js/aluno.js
+++ static/js/aluno.js
@@ -78,6 +78,10 @@ for (let turma of turmas) {
                     editTel.value = alunoObj.telefone
                     editEmail.value = alunoObj.email
 
+                    n1.value = alunoObj.notas[0] || 0
+                    n2.value = alunoObj.notas[1] || 0
+                    n3.value = alunoObj.notas[2] || 0 
+
                     subInfo.addEventListener("click", (event) => {
                         for (let i = 0; i < turmaObj.alunos.length; i++) {
                             if(turmaObj.alunos[i].matricula == codigoAluno) {
@@ -96,7 +100,7 @@ for (let turma of turmas) {
                     submit.addEventListener("click", (event) => {
                         for (let i = 0; i < turmaObj.alunos.length; i++) {
                             if(turmaObj.alunos[i].matricula == codigoAluno) {
-                                var alunoObj = Object.assign(new Aluno("",""), turmaObj.alunos[i])
+                                var alunoObj = Object.assign(new Aluno("",""), turmaObj.alunos[i])                              
 
                                 if(n1.value < 0 || n1.value > 10){                                           
                                     return alert("Nota Invalida")
