83770dee626ea985002387850b8e521613fa535f
ViniBilk <vinibilk4@gmail.com>
2021-07-13 22:30:29 -0300

    Editar informações, css aluno, tabela entre outros...

---
+++ static/js/aluno.js
@@ -0,0 +1,119 @@
+const codigoTurma = +new URLSearchParams(location.search).get("codigoTurma")
+const codigoAluno = +new URLSearchParams(location.search).get("codigoAluno")
+const turmas = JSON.parse(localStorage.getItem("turmasVetor"))
+
+var n1 = document.querySelector("#n1"),
+    n2 = document.querySelector("#n2"),
+    n3 = document.querySelector("#n3"),
+    submit = document.querySelector("#submit")
+
+ //Modal Info
+ var modalInfo = document.querySelector("#modalInfo");
+var btnInfo = document.querySelector("#btnInfo");
+var spanInfo = document.getElementsByClassName("exit")[0];
+
+btnInfo.addEventListener("click", (event) => {
+    modalInfo.style.display = "block";
+})
+
+spanInfo.addEventListener("click", (event) =>{
+    modalInfo.style.display = "none";
+}) 
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+} 
+
+//Modal Cadastro
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+modal.style.display = "block";
+}
+
+span.onclick = function() {
+modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+} 
+
+
+for (let turma of turmas) {
+    if (+turma.codigoTurma === codigoTurma) {
+        var turmaObj = Object.assign(new Turma("",""), turma)
+
+        for(let aluno of turmaObj.alunos){
+            if(aluno.matricula == codigoAluno){
+                var alunoObj = Object.assign(new Aluno("",""), aluno)
+                const infoAluno = document.createElement("p")
+                let info = document.querySelector(".info")
+                infoAluno.innerHTML = "Nome do aluno: " + (aluno.nome) + "<br>" + "Matricula: " + (aluno.matricula) + "<br>" + "Telefone: " + (aluno.telefone) + "<br>" + "E-mail: " + (aluno.email)
+                info.append(infoAluno)
+            
+
+                let subInfo = document.querySelector("#submitInfo"),
+                    editNome = document.querySelector("#editNome"),
+                    editTel = document.querySelector("#editTel"),
+                    editEmail = document.querySelector("#editEmail")
+        
+                let n1Nota = document.querySelector("#n1Nota"),
+                    n2Nota = document.querySelector("#n2Nota"),
+                    n3Nota = document.querySelector("#n3Nota"),
+                    mediaNota = document.querySelector("#mediaNota")
+
+                    n1Nota.innerHTML = alunoObj.notas[0] || "N/a"
+                    n2Nota.innerHTML = alunoObj.notas[1] || "N/a"
+                    n3Nota.innerHTML = alunoObj.notas[2] || "N/a"
+                    mediaNota.innerHTML = alunoObj.calcularMedia(+alunoObj.notas[0], +alunoObj.notas[1], +alunoObj.notas[2])
+
+                    editNome.value = alunoObj.nome
+                    editTel.value = alunoObj.telefone
+                    editEmail.value = alunoObj.email
+
+                    subInfo.addEventListener("click", (event) => {
+                        for (let i = 0; i < turmaObj.alunos.length; i++) {
+                            if(turmaObj.alunos[i].matricula == codigoAluno) {
+                                var alunoObj = Object.assign(new Aluno("",""), turmaObj.alunos[i])
+                                alunoObj.nome = editNome.value
+                                alunoObj.telefone = editTel.value
+                                alunoObj.email = editEmail.value
+                                turmaObj.alunos[i] = alunoObj
+     
+                                localStorage.setItem("turmasVetor",JSON.stringify(turmas))
+                                document.location.reload(true);
+                            }   
+                        }
+                    })
+
+                    submit.addEventListener("click", (event) => {
+                        for (let i = 0; i < turmaObj.alunos.length; i++) {
+                            if(turmaObj.alunos[i].matricula == codigoAluno) {
+                                var alunoObj = Object.assign(new Aluno("",""), turmaObj.alunos[i])
+
+                                if(n1.value < 0 || n1.value > 10){                                           
+                                    return alert("Nota Invalida")
+                                } else if(n2.value < 0 || n2.value > 10){                                         
+                                    return alert("Nota Invalida")
+                                }else if(n3.value < 0 || n3.value > 10){                                           
+                                    return alert("Nota Invalida")
+                                }else{
+                                    alunoObj.cadastraNotas(n1.value, n2.value, n3.value)
+                                    turmaObj.alunos[i] = alunoObj
+                                }                                                                                          
+                                localStorage.setItem("turmasVetor",JSON.stringify(turmas))
+                                document.location.reload(true);
+                            }   
+                        }
+                   })
+                }
+            }
+        }
+    } 
---
+++ static/js/alunos.js
@@ -0,0 +1,46 @@
+const codigoTurma = +new URLSearchParams(location.search).get("codigoTurma")
+const turmas = JSON.parse(localStorage.getItem("turmasVetor"))
+
+let erro = document.querySelector("#erro")
+
+let matricula = document.querySelector("#matricula"),
+    nome =  document.querySelector("#nome"),
+    telefone =  document.querySelector("#telefone"),
+    email =  document.querySelector("#email"),
+    submit = document.querySelector("#submit")
+
+for (let turma of turmas) {
+    if (+turma.codigoTurma === codigoTurma) {
+        var turmaObj = Object.assign(new Turma("",""), turma)
+
+        if(turmaObj.alunos == 0){
+            erro.innerHTML = "Não há alunos cadastrados"
+            erro.style.display = "block"
+        }
+
+        submit.addEventListener("click", (event) => {
+
+            for(let i = 0;i < turmaObj.alunos.length;i++){
+                if(+matricula.value == +turmaObj.alunos[i].matricula){
+                    alert("Matricula já utilizada")
+                    return
+                }
+            }
+            turmaObj.cadastrarAluno(matricula.value, nome.value, telefone.value, email.value)
+            localStorage.setItem("turmasVetor",JSON.stringify(turmas))
+            document.location.reload(true);
+        })
+
+        window.addEventListener("load", (event) =>{
+            let div = document.querySelector(".alunos")
+                for (let aluno of turma.alunos) {
+                    const linkAluno = document.createElement("a")
+
+                    linkAluno.href = `aluno.html?codigoTurma=${codigoTurma}&codigoAluno=${aluno.matricula}`
+                    linkAluno.innerHTML = "Nome do aluno: " + (aluno.nome) + "<br>" + "Matricula: " + (aluno.matricula) + "<br>"
+
+                    div.append(linkAluno)
+                }
+            })
+        }        
+    } 
